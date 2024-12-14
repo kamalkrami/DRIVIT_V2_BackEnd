ce716d98c706943f8f5832891f3b2479076f6069
kamal <kamalkrami4@gmail.com>
2024-12-14 22:28:40 +0100

    Second Commit V1.8 : More Configuration

--- config-services/src/main/java/net/kamal/configservices/ConfigServicesApplication.java
+++ config-services/src/main/java/net/kamal/configservices/ConfigServicesApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServicesApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ConfigServicesApplication.class, args);
     }
-
 }
