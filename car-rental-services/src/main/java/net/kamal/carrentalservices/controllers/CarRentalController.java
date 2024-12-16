3f8796ab3ec0f9846b6314a8fcfae90f166c5999
AyoubAnej <alyapani9@gmail.com>
2024-12-16 23:08:30 +0100

    add updateCarRental method for CarRental Service

--- car-rental-services/src/main/java/net/kamal/carrentalservices/controllers/CarRentalController.java
+++ car-rental-services/src/main/java/net/kamal/carrentalservices/controllers/CarRentalController.java
@@ -79,4 +79,34 @@ public class CarRentalController {
             ));
         }
     }
+
+    @PutMapping("carrental/updatecarrental")
+    public ResponseEntity<Map<String, Object>> updateCarRental(@RequestBody CarRental carRental){
+        //to check later
+        if (carRental ==  null || carRental.getId_car() == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of(
+                    "msg", "CarRental or CarRental ID cannot be null",
+                    "status", 404
+            ));
+        }
+        CarRental updatedCar = carRentalRepository.findById(carRental.getId_car()).orElse(null);
+        if(updatedCar == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of(
+                    "msg", "CarRental not found",
+                    "status", 404
+            ));
+        }
+        updatedCar.setId_car(carRental.getId_car());
+        updatedCar.setCars(carRental.getCars());
+        updatedCar.setId_user(carRental.getId_user());
+        updatedCar.setUsers(carRental.getUsers());
+        updatedCar.setRentalTime(carRental.getRentalTime());
+        updatedCar.setStatusRental(carRental.getStatusRental());
+
+        carRentalRepository.save(updatedCar);
+        return ResponseEntity.status(HttpStatus.CREATED).body(Map.of(
+                "msg", "CarRental has been updated successfully",
+                "status", 201
+        ));
+    }
 }
